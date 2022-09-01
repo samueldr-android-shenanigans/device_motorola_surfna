694d21f9244c791e00dbabb1c9cd4f781779bc85
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-09-01 13:45:22 -0400

    lineage_surfna.mk: Misc changes to look closer to LineageOS trees

--- lineage_surfna.mk
+++ lineage_surfna.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from surfna device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+$(call inherit-product, device/motorola/surfna/device.mk)
 
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := surfna
